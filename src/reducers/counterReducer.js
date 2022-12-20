865bbd3c6f2085497f004d220c8ab43f0156ae6e
ansariashif14 <114073325+ansariashif14@users.noreply.github.com>
2022-12-20 12:40:55 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,14 @@
 const counterReducer = (state,action) =>{
-
+  switch(action)
+  {
+    case "INCREMENT":
+      return state+1;
+    case "DECREMENT";
+      return state-1;
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
 }
-
 export {counterReducer}
